46aa1596e8569ff36ee36b443844cd26ec8bafaa
KARINA AUDAY <karina.auday@ort.edu.ar>
2023-08-23 21:08:49 -0300

    Creacion del Proyecto

---
+++ 2023-2C-F-Estacionamiento/2023-2C-F-Estacionamiento/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2023_2C_F_Estacionamiento.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PruebasKari/PruebasKari/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter URLs (comma-separated): ");
+        string[] urls = Console.ReadLine().Split(',');
+
+        foreach (string url in urls)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    string content = client.DownloadString(url);
+                    Console.WriteLine($"Content of {url}:\n{content}\n");
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine($"Error downloading {url}: {ex.Message}\n");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while processing {url}: {ex.Message}\n");
+            }
+        }
+    }
+}
+
+
+
+
+
