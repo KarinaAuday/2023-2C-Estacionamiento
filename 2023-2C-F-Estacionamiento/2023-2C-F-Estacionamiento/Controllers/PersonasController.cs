e94279efb2b340be280ac8d84803156e9e9446a4
KARINA AUDAY <karina.auday@ort.edu.ar>
2023-09-04 18:56:39 -0300

    Agrego Personas Controller

---
+++ 2023-2C-F-Estacionamiento/2023-2C-F-Estacionamiento/Controllers/PersonasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _2023_2C_F_Estacionamiento.Controllers
+{
+    public class PersonasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
