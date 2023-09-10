1a416a990b3da7be83122c4f04255deacf6357c8
KARINA AUDAY <karina.auday@ort.edu.ar>
2023-09-10 20:44:32 -0300

    Adecuacion Clase Startup

--- 2023-2C-F-Estacionamiento/2023-2C-F-Estacionamiento/Program.cs
+++ 2023-2C-F-Estacionamiento/2023-2C-F-Estacionamiento/Program.cs
@@ -1,27 +1,10 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+using _2023_2C_F_Estacionamiento;
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+var app = Startup.InicializarApp(args); // Pasamos los argunmentos que son recibidos en la ejecucion
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
+
---
+++ 2023-2C-F-Estacionamiento/2023-2C-F-Estacionamiento/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace _2023_2C_F_Estacionamiento
+{
+    public static class Startup
+    {
+        public static WebApplication InicializarApp (string[] args)
+        {
+            //Crear una nueva instancia de nuestro Servidor Web
+            var builder = WebApplication.CreateBuilder(args);
+
+            ConfigureServices(builder);//Lo configuramos con sus respectivos servicios
+
+            var app = builder.Build();
+
+            Configure(app);
+
+            return app; 
+
+        }
+        private static void ConfigureServices(WebApplicationBuilder Builder)
+        {
+            Builder.Services.AddControllersWithViews();
+
+        }
+
+        private static void Configure (WebApplication app)
+        {
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+
+    }
+}
