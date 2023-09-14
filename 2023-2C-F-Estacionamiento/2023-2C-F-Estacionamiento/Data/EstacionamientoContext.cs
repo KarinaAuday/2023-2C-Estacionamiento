01b6aff09d5a69db1672cda8823a8a5de8be0c5c
KARINA AUDAY <karina.auday@ort.edu.ar>
2023-09-13 22:07:13 -0300

    Agregue el Contexto de BD

---
+++ 2023-2C-F-Estacionamiento/2023-2C-F-Estacionamiento/Data/EstacionamientoContext.cs
@@ -0,0 +1,22 @@
+﻿using _2023_2C_F_Estacionamiento.Models;
+using Microsoft.EntityFrameworkCore;
+namespace _2023_2C_F_Estacionamiento.Data
+   
+{
+    public class EstacionamientoContext : DbContext
+    {
+        public EstacionamientoContext (DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Persona> Personas { get; set; }
+
+        public DbSet<Direccion> Direcciones { get; set; }    
+
+
+        
+        
+    }
+}
---
+++ 2023-2C-F-Estacionamiento/2023-2C-F-Estacionamiento/Startup.cs
@@ -0,0 +1,8 @@
+﻿namespace _2023_2C_F_Estacionamiento
+{
+    public class Startup
+    {
+
+
+    }
+}
